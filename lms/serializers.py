84da99f426419def2344ce3d4677d653a5e4eb05
Nikita Danilov <danilovn1@mail.ru>
2025-11-04 21:14:14 +0500

    hm1 v1

--- lms/serializers.py
+++ lms/serializers.py
@@ -2,11 +2,16 @@ from rest_framework import serializers
 from .models import Course, Lesson
 
 class CourseSerializer(serializers.ModelSerializer):
+    lessons_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
     class Meta:
         model = Course
-        fields = '__all__'
+        fields = ['id', 'title', 'preview', 'description', 'lessons_count']
+
+    def get_lessons_count(self, obj):
+        return obj.lessons.count()
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
-        fields = '__all__'
+        fields = ['id', 'title', 'description', 'preview', 'video_url']
